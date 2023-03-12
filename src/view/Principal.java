430da9123002840424ec4456196b12594ec208da
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-03-12 11:36:52 -0300

    feat: Principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,9 +1,18 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.ThreadPessoa;
+
 public class Principal {
 
 	public static void main(String[] args) {
-
+		Semaphore mutex = new Semaphore(1);
+		
+		for (int i = 1; i <= 4; i++) {
+			Thread pessoa = new ThreadPessoa(i, mutex);
+			pessoa.start();
+		}
 	}
 
 }
